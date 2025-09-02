dbce9b6f9b23a6aaf6e6ba750811a3068edf3ffa
Carolina Russi <carolinaferlarussi@gmail.com>
2025-09-02 13:10:52 -0300

    day 17 - always validate never trust the user

--- example/resources/views/jobs/create.blade.php
+++ example/resources/views/jobs/create.blade.php
@@ -13,8 +13,11 @@
                         <label for="title" class="block text-sm/6 font-medium text-gray-900">Job Title</label>
                         <div class="mt-2">
                             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="title" type="text" name="title" placeholder="Developer Engineer" class="block min-w-0 grow bg-white py-1.5 px-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                                <input id="title" type="text" name="title" placeholder="Developer Engineer" class="block min-w-0 grow bg-white py-1.5 px-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required />
                             </div>
+                            @error('title')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
@@ -22,17 +25,30 @@
                         <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary (Per Year)</label>
                         <div class="mt-2">
                             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="salary" type="text" name="salary" placeholder="50.000" class="block min-w-0 grow bg-white py-1.5 px-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                                <input id="salary" type="text" name="salary" placeholder="50.000" class="block min-w-0 grow bg-white py-1.5 px-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required />
                             </div>
+                            @error('salary')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-        </div>
+                <!-- Para mostrar todos os erros
+                <div class="mt-10">
+                    @if($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li class="text-red-500 text-sm">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+                </div> -->
+            </div>
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            </div>
     </form>
 </x-layout>
