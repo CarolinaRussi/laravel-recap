ce8d13e13606138fed3e4a62db176038e91c624a
Carolina Russi <carolinaferlarussi@gmail.com>
2025-09-05 10:44:30 -0300

    day 21 - login and registration from scratch

--- example/routes/web.php
+++ example/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -15,3 +18,10 @@ Route::resource(
     //, ['except' => ['show']] --Para quando quero que alguma rota nao seja criada
     //, ['only' => ['index', 'show']] --Para quando quero que apenas algumas rotas sejam criadas
 );
+
+//Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
