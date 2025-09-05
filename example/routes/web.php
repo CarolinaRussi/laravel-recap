b658a9142638bfbe0019027ecd2836ef2b326ecb
Carolina Russi <carolinaferlarussi@gmail.com>
2025-09-04 21:17:37 -0300

    day 20 - Starter kits, Breeze and Middleware

--- example/routes/web.php
+++ example/routes/web.php
@@ -7,8 +7,11 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 //Jobs Routes (where the magic happens)
-Route::resource('jobs', JobController::class
+Route::resource(
+    'jobs',
+    JobController::class
+    // Com o resource, o Laravel cria todas as rotas automaticamente
+    // Sendo elas: index, create, store, show, edit, update, destroy
     //, ['except' => ['show']] --Para quando quero que alguma rota nao seja criada
     //, ['only' => ['index', 'show']] --Para quando quero que apenas algumas rotas sejam criadas
 );
-
