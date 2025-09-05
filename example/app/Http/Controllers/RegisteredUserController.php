cbe2473a7ef13990c1610bc2dd626bdcc2c7874a
Carolina Russi <carolinaferlarussi@gmail.com>
2025-09-05 11:23:29 -0300

    day 22 - login and registration from scratch 2

--- example/app/Http/Controllers/RegisteredUserController.php
+++ example/app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +15,21 @@ class RegisteredUserController extends Controller
 
     public function store(Request $request)
     {
-        dd(request()->all());
+        // validate
+        $attributes = request()->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:6', // when using confirmed laravel looks for a field with _confirmation
+        ]);
+
+        // create the user
+        $user = User::create($attributes);
+
+        // login
+        Auth::login($user);
+
+        // redirect
+        return redirect('/jobs');
     }
 }
--- example/app/Http/Controllers/SessionController.php
+++ example/app/Http/Controllers/SessionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -12,6 +14,28 @@ class SessionController extends Controller
     }
     public function store(Request $request)
     {
-        dd(request()->all());
+        // validate
+        $attributes = request()->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        
+        // attempt to authenticate and login the user
+        if(!Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'The provided credentials do not match our records.',
+            ]);
+        };
+
+        // regenarate the session token
+        request()->session()->regenerate();
+        
+        //redirect
+        return redirect('/jobs');
+    }
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect('/');
     }
 }
